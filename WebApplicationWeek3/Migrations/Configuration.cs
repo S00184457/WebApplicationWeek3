63ea96ec477d9a3ecbc44df3a5e08c135fd5b840
Hugo Pilon <S00184457@mail.itsligo.ie>
2018-10-05 11:29:56 +0100

    Second Migration and Application Users seeded

--- WebApplicationWeek3/Migrations/Configuration.cs
+++ WebApplicationWeek3/Migrations/Configuration.cs
@@ -1,9 +1,12 @@
 namespace WebApplicationWeek3.Migrations
 {
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
+    using WebApplicationWeek3.Models;
 
     internal sealed class Configuration : DbMigrationsConfiguration<WebApplicationWeek3.Models.ApplicationDbContext>
     {
@@ -15,10 +18,59 @@ namespace WebApplicationWeek3.Migrations
 
         protected override void Seed(WebApplicationWeek3.Models.ApplicationDbContext context)
         {
-            //  This method will be called after migrating to the latest version.
+            var manager =
+                new UserManager<ApplicationUser>(
+                    new UserStore<ApplicationUser>(context));
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data.
+            var roleManager =
+                new RoleManager<IdentityRole>(
+                    new RoleStore<IdentityRole>(context));
+
+            context.Roles.AddOrUpdate(r => r.Name,
+                new IdentityRole { Name = "Admin" }
+                );
+            context.Roles.AddOrUpdate(r => r.Name,
+                new IdentityRole { Name = "ClubAdmin" }
+                );
+            context.Roles.AddOrUpdate(r => r.Name,
+                new IdentityRole { Name = "member" }
+                );
+
+            PasswordHasher ps = new PasswordHasher();
+
+            context.Users.AddOrUpdate(u => u.UserName,
+                new ApplicationUser
+                {
+                    UserName = "Admin",
+                    Email = "powell.paul@itsligo.ie",
+                    EmailConfirmed = true,
+                    JoinDate = DateTime.Now,
+                    SecurityStamp = Guid.NewGuid().ToString(),
+                    FirstName = "Paul",
+                    SurName = "Powell",
+                    PasswordHash = ps.HashPassword("Ppowell$1")
+                });
+
+            context.Users.AddOrUpdate(u => u.UserName,
+                new ApplicationUser
+                {
+                    UserName = "ITS FC Admin",
+                    Email = "radp2016@outlook.com",
+                    EmailConfirmed = true,
+                    JoinDate = DateTime.Now,
+                    SecurityStamp = Guid.NewGuid().ToString(),
+                    FirstName = "Rad",
+                    SurName = "Paulner",
+                    PasswordHash = ps.HashPassword("radP2016$1")
+                });
+            context.SaveChanges();
+
+            ApplicationUser admin = manager.FindByEmail("powell.paul@itsligo.ie");
+            if (admin != null)
+            {
+                manager.AddToRoles(admin.Id, new string[] { "Admin", "member", "ClubAdmin" });
+            }
+
         }
     }
 }
