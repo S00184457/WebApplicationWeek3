5bb1f34a6e668ac00487a64b5d9a7ffeb4232167
Hugo Pilon <S00184457@mail.itsligo.ie>
2018-10-05 10:37:03 +0100

    Add project files.

---
+++ WebApplicationWeek3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApplicationWeek3.MvcApplication" Language="C#" %>
---
+++ WebApplicationWeek3/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApplicationWeek3.Startup))]
+namespace WebApplicationWeek3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
