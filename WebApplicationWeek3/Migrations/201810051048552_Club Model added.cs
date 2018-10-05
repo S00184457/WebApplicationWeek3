bd096c59b15387b16ecd9bcefc3455dde2bc27a9
Hugo Pilon <S00184457@mail.itsligo.ie>
2018-10-05 11:51:11 +0100

    added club model

---
+++ WebApplicationWeek3/Migrations/201810051048552_Club Model added.cs
@@ -0,0 +1,16 @@
+namespace WebApplicationWeek3.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ClubModeladded : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
